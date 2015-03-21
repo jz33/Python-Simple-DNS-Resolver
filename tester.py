bc9cd78d046b13f3176b50f7117e63621fb5a04a
Jun <junzhengrice@gmail.com>
2015-03-21 10:21:35 -0500

    Tester is able to execute 'A' and 'MX'

--- tester.py
+++ tester.py
@@ -2,23 +2,33 @@ import DNSMessage
 import DNSResolver
 import struct
 
-question = 'google.com'
-rr = 'MX'
 ACK = chr(6)
 
-data = DNSMessage.Header() + ACK + DNSMessage.Question(question,rr)
-for w in data: print ord(w),
-print
+def composeData(question, rr):
+    data = DNSMessage.Header() + ACK + DNSMessage.Question(question,rr) 
+    for w in data: print ord(w),
+    print
+    return data    
 
-resolver = DNSResolver.Resolver(data = data)
-resolver.Connect()
-resolver.Send()
-try:
+
+def execute(resolver,question,type):
+    data = composeData(question,type)
+    resolver.UpdateData(data)
+    resolver.Send()
     chunk = resolver.Receive()
     DNSMessage.Answer(chunk)
-finally:
-    resolver.Disconnect()
+    print
+        
+def main():
+    question = 'google.com'
     
-
-    
-
+    resolver = DNSResolver.Resolver()
+    resolver.Connect()
+    try:
+        execute(resolver,question,'A')
+        execute(resolver,question,'MX')
+    finally:
+        resolver.Disconnect()
+        
+if __name__ == '__main__':
+    main()
